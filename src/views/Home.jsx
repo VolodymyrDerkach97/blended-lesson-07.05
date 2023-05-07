5e6ecfd16cc91ef1f9996947d6d499d6a8a88498
Volodymyr Derkach <bobikderkac@gmail.com>
2023-05-07 11:06:01 +0300

    add routes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,26 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { useEffect, useState } from "react";
+import { getTrendingCocktails } from "../api/cocktail-service";
 
 export const Home = () => {
+  const [cocktails, setCocktails] = useState(null);
+
+  useEffect(() => {
+    getTrendingCocktails().then((res) => {
+      setCocktails(res.map((item) => item.drinks[0]));
+    });
+  }, []);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
 
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        <CocktailsList cocktails={cocktails} />
       </Section>
     </>
   );
